d534514e9fd2047fe9523d817b5d0947f2294b4e
amirrezaask <raskarpour@gmail.com>
2023-07-08 22:34:14 +0330

    remove binary since we have test

--- bin/main.ml
+++
@@ -1,8 +0,0 @@
-open Base;;
-open Stdio;;
-
-let () =
-  match Request.from_string "GET http://localhost:1323/api/v2/asset/167\nAuthorization: Bearer {{jwtToken}}\nContent-Type: application/json\n\n{\"name\": \"amirreza\"}" with
-  | Ok req -> printf "%s" (Request.show req)
-  | Error msg -> Stdlib.Printf.printf "%s" msg
-;;
